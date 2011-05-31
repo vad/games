99a609f64151d7f75c77db67cfe5b6b0961c88ea
Davide Setti <davide.setti@gmail.com>
2011-05-31 22:07:32 +0200

    sounds from Sonatina Symphonic Orchestra

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ from pygame import *
 import pygame, time, numpy, pygame.sndarray
 import sys
 import random
+from glob import glob
 
 sample_rate = 44100
 
@@ -11,26 +12,7 @@ sample_rate = 44100
 class Player(object):
     def __init__(self, surface):
         self.surface = surface
-        self._clean()
-
-    def _play_for(self, sample_array, ms, volLeft, volRight):
-        sound = pygame.sndarray.make_sound(sample_array)
-        beg = time.time()
-        channel = sound.play(-1)
-        channel.set_volume(volLeft,volRight)
-        pygame.time.delay(ms)
-        sound.stop()
-        end = time.time()
-        return beg, end
-
-    def _sine_array_onecycle(self, hz, peak):
-        length = sample_rate / float(hz)
-        omega = numpy.pi * 2 / length
-        xvalues = numpy.arange(int(length)) * omega
-        return peak * numpy.sin(xvalues)
-
-    def _sine_array(self, hz, peak, n_samples = sample_rate):
-        return numpy.resize(self._sine_array_onecycle(hz, peak), (n_samples,))
+        self.clean()
 
     def _draw(self, color=None):
         randint = random.randint
@@ -41,28 +23,27 @@ class Player(object):
         pygame.draw.circle(self.surface, color, (x, y), radius)
         pygame.display.update()
 
-    def _clean(self):
+    def clean(self):
         self.surface.fill((0, 0, 0))
         pygame.display.update()
 
-    def play(self, freq):
-        f = self._sine_array(freq, 1)
-        f = numpy.array(zip(f , f))
-
+    def play(self, sound):
         self._draw()
-        self._play_for(f , 500, 0.5, 0.5)
-        self._clean()
+        pygame.mixer.music.load(sound)
+        pygame.mixer.music.play()
 
 
 def main():
     pygame.mixer.pre_init(sample_rate, -16, 2) # 44.1kHz, 16-bit signed, stereo
     pygame.init()
     surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.mixer.music.set_endevent(pygame.USEREVENT)
     player = Player(surface)
 
     random.seed(0.5)
-    shuffler = range(200)
+    shuffler = glob("Samples/*/*.wav")
     random.shuffle(shuffler)
+    shuffler = shuffler[:200]
 
     while True:
         event = pygame.event.wait()
@@ -71,13 +52,16 @@ def main():
 
             if key == 99 and mod & pygame.KMOD_CTRL:
                 sys.exit()
+
+            sound = shuffler[key%200]
+            player.play(sound)
         elif event.type == pygame.QUIT:
             sys.exit(0)
-        else:
-            continue
+        elif event.type == pygame.USEREVENT:
+            #player.clean()
+            pass
 
-        freq = 3000 + int(shuffler[key%200] * 4000. / 200)
-        player.play(freq)
+
 
 
 if __name__ == '__main__':
