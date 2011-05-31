984339904211b6950a59a12994e04a8a05a07d6e
Davide Setti <davide.setti@gmail.com>
2011-05-31 20:57:10 +0200

    draw some circles

--- player.py
+++ player.py
@@ -11,6 +11,7 @@ sample_rate = 44100
 class Player(object):
     def __init__(self, surface):
         self.surface = surface
+        self._clean()
 
     def _play_for(self, sample_array, ms, volLeft, volRight):
         sound = pygame.sndarray.make_sound(sample_array)
@@ -31,11 +32,26 @@ class Player(object):
     def _sine_array(self, hz, peak, n_samples = sample_rate):
         return numpy.resize(self._sine_array_onecycle(hz, peak), (n_samples,))
 
+    def _draw(self, color=None):
+        randint = random.randint
+        w, h = self.surface.get_size()
+        x, y, radius = randint(0, w), randint(0, h), randint(10, 100)
+        if not color:
+            color = randint(50, 255), randint(50, 255), randint(50, 255)
+        pygame.draw.circle(self.surface, color, (x, y), radius)
+        pygame.display.update()
+
+    def _clean(self):
+        self.surface.fill((0, 0, 0))
+        pygame.display.update()
+
     def play(self, freq):
         f = self._sine_array(freq, 1)
         f = numpy.array(zip(f , f))
 
-        self._play_for(f , 100, 0.5, 0.5)
+        self._draw()
+        self._play_for(f , 500, 0.5, 0.5)
+        self._clean()
 
 
 def main():
